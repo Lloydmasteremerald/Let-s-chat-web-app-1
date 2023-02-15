f34808e981de2b0b3614dbe1874a581518a2c4d4
Lloydmasteremerald <110808538+Lloydmasteremerald@users.noreply.github.com>
2023-02-15 18:32:17 +1300

    Add files via upload

--- chat.js
+++ chat.js
@@ -1,6 +1,15 @@
 // Your web app's Firebase configuration
 //ADD YOUR FIREBASE LINKS HERE
-
+const firebaseConfig = {
+    apiKey: "AIzaSyBOohT5Bp5v9riG-mcsN198T_w8JjzRNZc",
+    authDomain: "practice-8f7c8.firebaseapp.com",
+    databaseURL: "https://practice-8f7c8-default-rtdb.firebaseio.com",
+    projectId: "practice-8f7c8",
+    storageBucket: "practice-8f7c8.appspot.com",
+    messagingSenderId: "48193162606",
+    appId: "1:48193162606:web:e712e8303d62683aa9457f",
+    measurementId: "G-040L2H7CS3"
+  };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
